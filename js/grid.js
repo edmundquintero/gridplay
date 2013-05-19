8ec60a4060fb8bf67a58dc1181b1e2f2ea2ccd8f
unknown <edmund.quintero@cpcc.edu>
2013-05-19 01:37:24 -0400

    Changed event handlers to the .on opperator

--- js/grid.js
+++ js/grid.js
@@ -1,36 +1,49 @@
 $(function(){
-
-    var square = '<div class="square" style="height: 10%; width: 9.7%;"></div>'
+    var clicking = false;
+    var square = '<div class="square grass"></div>'
     var x = 100;
     for(i=0; i<x; i++){
         $('#grid').append(square);
     }
-
-
-    $('.square').hover(
-        function(){
+    count();
+    $('.square').on({
+        mouseenter:  function(){
             $(this).addClass('highlight');
         },
-        function(){
+        mouseleave: function(){
             $(this).removeClass('highlight');
+        }
         });
 
-    $('.square').click(function(){
+    $('.square').on('click', function(){
         if($(this).hasClass('selected')){
             $(this).removeClass('selected');
         }else{
             $(this).addClass('selected');
         }
-        count();
     });
 
-    $('#clear-all').on('click', function(){
+    $('#clear-selected').on('click', function(){
         $('.square').removeClass('selected');
         count();
     });
 
+    $('.world-item').on('click', function(){
+        var item = $(this).attr('ref');
+        $('.selected').attr('class','square '+item);
+        $('#clear-selected').trigger('click');
+    });
+
+    $('#reset').on('click', function(){
+        $('.square').attr('class','square grass');
+        count();
+    });
 });
 
 function count(){
     $('#total-selected').html($('.selected').length);
+    $('#total-grass').html($('.grass').length);
+    $('#total-rock').html($('.rock').length);
+    $('#total-water').html($('.water').length);
+    $('#total-tree').html($('.tree').length);
 };
